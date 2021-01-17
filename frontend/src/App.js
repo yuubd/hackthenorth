907a0865c5748e3882bc9418c8aeb58968bd30bc
Alice <aliceykim0828@gmail.com>
2021-01-17 01:42:21 -0800

    implement dark mode (#17)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,37 @@
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 
+const theme = {
+    light: {
+        background: 'inherit',
+        color: 'inherit',
+    },
+    dark: {
+        background: '#343434',
+        color: 'white',
+    }
+}
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        background: ${({ theme }) => theme.background};
+        color: ${({ theme }) => theme.color};
+        font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
+        transition: all 0.50s linear;
+    }
+`
+
 function App() {
+    const [dark, setDark] = useState(false)
+
     return (
-        <ThemeProvider theme={{ fontFamily: 'Quicksand, sans-serif'}}>
+        <ThemeProvider theme={dark ? theme.dark : theme.light}>
+            <GlobalStyle />
             <Router>
-                <Navbar />
+                <Navbar dark={dark} setDark={setDark} />
                 <Switch>
                     <Route exact path="/">
                         <Home />
